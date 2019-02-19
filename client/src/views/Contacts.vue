1b22b06eb096dbdf62aedad12c73f2efb640b83e
lipeng <lipeng@daokoudai.com>
2019-02-19 21:23:21 +0800

    15.VueCli3.x实战项目-通讯录搜索功能

--- client/src/views/Contacts.vue
+++ client/src/views/Contacts.vue
@@ -2,12 +2,22 @@
     .address_book
         Header(title='通讯录' btn_icon='user-plus')
         .container
+            //- 上面 搜索框
+            .search_wrap
+                .sear_content
+                    i.fa.fa-search
+                    input(placeholder='搜索' v-model='search_value')
+
+            //- 中间
             .content_wrap
                 Cell(
                     v-for='(friend,index) in friendsList'
                     :key='friend._id'
                     :user='friend'
                 )
+            //- 下面
+            .count_wrap
+                span {{friendsList.length}}位联系人
 </template>
 
 <script>
@@ -17,6 +27,8 @@ export default {
     data(){
         return {
             friendsList: [],
+            allFriends: [],
+            search_value: '',
         }
     },
     created(){
@@ -26,11 +38,76 @@ export default {
         Header,
         Cell,
     },
+    watch: {
+        search_value(){
+            this.filterData()
+        },
+    },
     methods: {
         getFriendsList(){
             this.$axios.get('/api/users/all')
-                .then(res => this.friendsList = res.data)
-        }
+                .then(res => {
+                    this.friendsList = res.data
+                    this.allFriends = res.data
+                })
+        },
+        filterData(){
+            this.friendsList = this.allFriends.filter(friend => {
+                return friend.name.indexOf(this.search_value) != -1
+            })
+        },
     },
 }
 </script>
+
+<style scoped>
+.address_book {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.container {
+  width: 100%;
+  height: calc(100% - 50px);
+  margin-top: 50px;
+  overflow: auto;
+}
+
+.search_wrap {
+  background-color: #f1f1f1;
+  padding: 8px;
+  box-sizing: border-box;
+  width: 100%;
+}
+.sear_content {
+  height: 40px;
+  background: #fff;
+  padding: 0 10px;
+  box-sizing: border-box;
+  font-size: 16px;
+  border: 1px solid #d9d9d9;
+  border-radius: 4px;
+}
+.sear_content i {
+  color: #888;
+  margin-right: 10px;
+}
+.sear_content input {
+  height: 36px;
+  width: 90%;
+  outline: none;
+  border: none;
+  font-size: 14px;
+}
+
+.count_wrap {
+  min-height: 50px;
+  background-color: #fff;
+  box-sizing: border-box;
+  color: #888;
+  font-size: 16px;
+  width: 100%;
+  line-height: 50px;
+  text-align: center;
+}
+</style>
