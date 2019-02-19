1694b6b0e94469e0391d8b44ebad30681a79ae2e
lipeng <lipeng@daokoudai.com>
2019-02-19 20:36:39 +0800

    14.VueCli3.x实战项目-获取所有用户列表

--- client/src/views/Contacts.vue
+++ client/src/views/Contacts.vue
@@ -1,13 +1,36 @@
 <template lang="pug">
-    div
+    .address_book
         Header(title='通讯录' btn_icon='user-plus')
+        .container
+            .content_wrap
+                Cell(
+                    v-for='(friend,index) in friendsList'
+                    :key='friend._id'
+                    :user='friend'
+                )
 </template>
 
 <script>
 import Header from '../components/Header'
+import Cell from '../components/UserCell'
 export default {
+    data(){
+        return {
+            friendsList: [],
+        }
+    },
+    created(){
+        this.getFriendsList()
+    },
     components: {
         Header,
+        Cell,
+    },
+    methods: {
+        getFriendsList(){
+            this.$axios.get('/api/users/all')
+                .then(res => this.friendsList = res.data)
+        }
     },
 }
 </script>
